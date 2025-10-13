5943ac18cbafc88132b894b92c54bd9b4c2924ed
Minh Thuận <thuantmse184066@fpt.edu.vn>
2025-10-13 14:00:07 +0700

    Commit dau tien

---
+++ src/main/java/sum25/se184066/pehsf302trialexamse184066/airplane_booking_group3/AirPlaneBookingGroup3Application.java
@@ -0,0 +1,13 @@
+package sum25.se184066.pehsf302trialexamse184066.airplane_booking_group3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AirPlaneBookingGroup3Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AirPlaneBookingGroup3Application.class, args);
+    }
+
+}
---
+++ src/test/java/sum25/se184066/pehsf302trialexamse184066/airplane_booking_group3/AirPlaneBookingGroup3ApplicationTests.java
@@ -0,0 +1,13 @@
+package sum25.se184066.pehsf302trialexamse184066.airplane_booking_group3;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AirPlaneBookingGroup3ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
